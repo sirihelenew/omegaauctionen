3456c42d218c5f6b35233d909c52c8b494f0eef7
sirihelenew <sirihwa@stud.ntnu.no>
2024-02-27 13:34:20 +0100

    added files and restructured directory

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -6,6 +6,7 @@
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 firebaseConfig = {
   apiKey: "AIzaSyDC_yGwhnlgbRTLdvvSJkNi-FGFZgzUKuk",
   authDomain: "omegaauctionen.firebaseapp.com",
